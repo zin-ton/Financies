bec24c7ebac54d9706cc41d5daa500d40c111462
zinton <zhilitskianton05@gmail.com>
2023-12-18 18:35:28 +0100

    Button

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -5,19 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextFieldDefaults.contentPadding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -29,10 +40,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
     }
 }
-
+private val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
 @Preview(showBackground = true, widthDp = 393, heightDp = 808)
 @Composable
 fun WhiteTheme() {
+    val lexend = GoogleFont("Lexend")
+    val lexendFamily = FontFamily(
+        Font(googleFont = lexend, fontProvider = provider)
+    )
     Row(
         Modifier
             .background(Color.White)
@@ -42,18 +61,25 @@ fun WhiteTheme() {
             Modifier
                 .background(Color.Red)
                 .fillMaxWidth()
-                .fillMaxSize(0.05F)
-                .padding(3.dp)
+                .fillMaxSize(0.04F)
         ) {
-            OutlinedButton(onClick = { /*TODO*/ },
+            Button(onClick = { /*TODO*/ },
                 colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.White,
+                    containerColor = Color(217, 217, 217, 255),
                     contentColor = Color.Black
-                )
+                ),
+                modifier = Modifier
+                    .width(75.dp),
+                contentPadding = PaddingValues(2.dp),
             ){
                 Text(
-                    "Main",
-                    fontSize = 12.sp
+                    "main",
+                    fontSize = 17.sp,
+                    fontFamily = lexendFamily,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight(300),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.CenterVertically)
                 )
             }
         }
