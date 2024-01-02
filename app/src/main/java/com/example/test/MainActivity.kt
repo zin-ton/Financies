3486008eca846a256e72e9223a17f1dad809d918
zinton <zhilitskianton05@gmail.com>
2024-01-02 19:26:58 +0100

    Card

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.test
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialogDefaults.containerColor
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -28,6 +41,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.text.font.FontFamily
@@ -41,6 +56,7 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.test.ui.theme.TestTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,17 +81,20 @@ fun WhiteTheme() {
     val lexendFamily = FontFamily(
         Font(googleFont = lexend, fontProvider = provider)
     )
-    Row(
+    Column(
         Modifier
-            .background(Color.White)
-            .fillMaxSize()
-            .padding(8.dp)) {
-        Column(
+            .background(Color.Red)
+            .wrapContentHeight()
+            .padding(4.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
             Modifier
-                .background(Color.Red)
+                .background(Color.White)
                 .fillMaxWidth()
                 .fillMaxSize(0.04F)
-                .padding(2.dp)
+                .padding(2.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Button(
                 onClick = {
@@ -103,7 +122,7 @@ fun WhiteTheme() {
                     modifier = Modifier.align(Alignment.CenterVertically)
                 )
             }
-            Button( //statistics button dont work
+            Button(
                 onClick = {
                     //TODO
                     mainContainerColor = grey
@@ -116,7 +135,7 @@ fun WhiteTheme() {
                     containerColor = statisticsContainerColor
                 ),
                 modifier = Modifier
-                    .width(75.dp),
+                    .width(100.dp),
                 contentPadding = PaddingValues(2.dp),
             ){
                 Text(
@@ -129,6 +148,79 @@ fun WhiteTheme() {
                     modifier = Modifier.align(Alignment.CenterVertically)
                 )
             }
+            Button(
+                onClick = {
+                    //TODO
+                    mainContainerColor = grey
+                    statisticsContainerColor = grey
+                    historyContainerColor = cyan
+                    remindersContainerColor = grey
+                },
+                colors = buttonColors(
+                    contentColor = Color.Black,
+                    containerColor = historyContainerColor
+                ),
+                modifier = Modifier
+                    .width(75.dp),
+                contentPadding = PaddingValues(2.dp),
+            ){
+                Text(
+                    "history",
+                    fontSize = 17.sp,
+                    fontFamily = lexendFamily,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight(300),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+            Button(
+                onClick = {
+                    //TODO
+                    mainContainerColor = grey
+                    statisticsContainerColor = grey
+                    historyContainerColor = grey
+                    remindersContainerColor = cyan
+                },
+                colors = buttonColors(
+                    contentColor = Color.Black,
+                    containerColor = remindersContainerColor
+                ),
+                modifier = Modifier
+                    .width(100.dp),
+                contentPadding = PaddingValues(2.dp),
+            ){
+                Text(
+                    "reminders",
+                    fontSize = 17.sp,
+                    fontFamily = lexendFamily,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight(300),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+        }
+        Spacer(Modifier.height(10.dp))
+    }
+}
+@Composable
+@Preview(showBackground = true, widthDp = 393, heightDp = 808)
+fun Card(){//TODO
+    val cyan = Color(198, 235,255, 255)
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.background(Color.White)
+            .fillMaxWidth(0.95f)
+            .wrapContentHeight(),
+    ) {
+        Column(
+            Modifier
+                .background(cyan)
+                .wrapContentHeight()
+                .fillMaxWidth(0.95f),
+        ) {
+            Text("adada")
         }
     }
 }
